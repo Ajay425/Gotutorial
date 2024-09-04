87a7201c04603479bf0bbc69fb5a357285322d3d
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2024-09-03 21:45:38 -0400

    learning go

---
+++ investment-calc.go
@@ -0,0 +1,28 @@
+package main  // tells go this is where the execution starts from
+
+import (
+	"fmt"
+	"math"
+)
+
+func main()  {
+	const inflationRate = 2.5
+	var inveestmentAmount float64 = 2000
+	var expectedReturn = 5.5
+	var years float64 = 5
+
+
+	var future = inveestmentAmount * math.Pow(1 + expectedReturn / 100, years)
+	inflationValue := future / math.Pow(1 + inflationRate / 100, years)
+	
+	fmt.Println(future)
+	fmt.Println(inflationValue)
+}
+
+/*
+Some things to know are:
+ - CANNOT USE single quotes in GO while passing a string, Can use `this type of string `
+ - Every GO code needs package
+ - Package vs Modules
+ - Two files cannot have a main func in the same package
+*/
