f2e1c7a132b3c46703bc7520731ce2c5f6441362
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2024-09-11 21:53:50 -0400

    refactor: calculate tax using multiple functions

--- gofiles/maingo.go
+++ gofiles/maingo.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main(){
-	fmt.Print("Hello World")
+)
+
+
+
+func main() {
+
+	revenue := askInput("Revenue: ")
+	expenses:= askInput("Expenses: ")
+	taxRate := askInput("Tax Rate: ")
+
+	//ebt := revenue - expenses
+	//profit := ebt * (1 - taxRate/100)
+	//ratio := ebt / profit
+	
+	ebt, profit, ratio := calculateTax(revenue, expenses, taxRate)
+
+
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+}
+
+func askInput(text string) float64{
+	var userInput float64
+	fmt.Print(text) 
+	fmt.Scan(&userInput)
+
+	return userInput
+}
+
+func calculateTax(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return
 }
